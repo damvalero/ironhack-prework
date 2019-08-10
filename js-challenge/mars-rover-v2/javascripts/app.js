f269a16b2fdcb857aa0d588782e4b8770abe18c5
Daniel Mendes Valero <dmendesvalero@gmail.com>
2019-08-10 23:39:24 +0100

    function backward and script things added

--- js-challenge/mars-rover-v2/javascripts/app.js
+++ js-challenge/mars-rover-v2/javascripts/app.js
@@ -23,15 +23,19 @@ function turnLeft(rover){
   switch(rover.direction){
     case 'N':
       rover.direction= 'W';
+      console.log('Rover direction: west');
       break;
     case 'W':
       rover.direction= 'S';
+      console.log('Rover direction: south');
       break;
     case 'S':
       rover.direction= 'E';
+      console.log('Rover direction: est');
       break
     case 'E':
       rover.direction= 'N';
+      console.log('Rover direction: north');
       break;      
   }
   console.log("turnLeft was called!");
@@ -41,15 +45,19 @@ function turnRight(rover){
   switch(rover.direction){
     case 'N':
       rover.direction= 'E';
+      console.log('Rover direction: est');
       break;
     case 'E':
       rover.direction= 'S';
+      console.log('Rover direction: south');
       break;
     case 'S':
       rover.direction= 'W';
+      console.log('Rover direction: west');
       break
     case 'W':
       rover.direction= 'N';
+      console.log('Rover direction: north');
       break;      
   }
   console.log("turnRight was called!");
@@ -90,8 +98,43 @@ function moveForward(rover){
   console.log('Rover is in the position: ' + [rover.x,rover.y]);
 }
 
+function moveBackward(rover){// similar to fordward but the move in each direction will be the oposite
+  switch(rover.direction){
+    case 'N':
+      if(rover.y>=10){
+        console.log('Is reached the limit space to move');
+      }else{
+        rover.y+=1;
+      }
+      break;
+    case 'E':
+      if (rover.x<=0){
+        console.log('Is reached the limit space to move');
+      }else{
+        rover.x-=1;
+      }
+      break;
+    case 'S':
+      if (rover.y<=0){
+        console.log('Is reached the limit space to move');
+      }else{
+        rover.y-=1;
+      }
+      break;
+    case 'W':
+      if (rover.x>=10){
+        console.log('Is reached the limit space to move');
+      }else{
+        rover.x+=1;
+      }
+      break;  
+  }
+  console.log("moveBackward was called");
+  console.log('Rover is in the position: ' + [rover.x,rover.y]);
+}
+
 function commands(route){
-  if (route.indexOf('l')>=0 || route.indexOf('r')>=0 || route.indexOf('f')>=0){
+  if (route.indexOf('l')>=0 || route.indexOf('r')>=0 || route.indexOf('f')>=0 || route.indexOf('b')>=0){
     for (let i=0; i<route.length; i++){
       switch(route[i]){
         case 'l':
@@ -102,17 +145,20 @@ function commands(route){
           break;
         case 'f':
           moveForward(rover);
-          break;    
+          break;
+        case 'b':
+          moveBackward(rover);
+          break;      
       }    
     }
     let position= [rover.x,rover.y];
     rover.travelLog.push(position);
     console.log('Rover final position is: ' + position);
   }else{
-    console.log('Enter valid caracters: l(left), r(right) or f(forward)');
+    console.log('Enter valid caracters: l(left), r(right), f(forward) or b(backward)');
   }
 }
 
-const route = 'rrfffffffffflfffffffffflfffffrllfzzzhhh';
+const route = 'llffffbbrfbbbbbbbbbbbggg' //'rrfffffffffflfffffffffflfffffrllfbb';
 commands(route);// to call the function
 
